3223a689fc06fb9079837f806d1aeb33b545e22c
NadiaAqmarina <nadia.10rpl2.2019@gmail.com>
2020-09-05 20:25:45 +0700

    add data online

--- app/src/main/java/com/example/richa11rpl22019/ListData.java
+++ app/src/main/java/com/example/richa11rpl22019/ListData.java
@@ -5,7 +5,17 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
+
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -20,7 +30,8 @@ public class ListData extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_data);
         recyclerView = (RecyclerView) findViewById(R.id.rvdata);
-        addData();
+//        addData();
+        addDataOnline();
     }
 
     void addData() {
@@ -50,5 +61,66 @@ public class ListData extends AppCompatActivity {
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(ListData.this);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(adapter);
+    }
+    void addDataOnline(){
+        AndroidNetworking.get("https://api.themoviedb.org/3/movie/now_playing?api_key=6ac7a042ac3b7599a689eb943fa0b6d0&language=en-US")
+                .setTag("test")
+                .setPriority(Priority.LOW)
+                .build()
+                .getAsJSONObject(new JSONObjectRequestListener() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // do anything with response
+                        Log.d("hasiljson", "onResponse: " + response.toString());
+                        //jika sudah berhasil debugm lanjutkan code dibawah ini
+                        DataArrayList = new ArrayList<>();
+                        Model modelku;
+                        try {
+                            Log.d("hasiljson", "onResponse: " + response.toString());
+                            JSONArray jsonArray = response.getJSONArray("results" +
+                                    "");
+                            Log.d("hasiljson2", "onResponse: " + jsonArray.toString());
+                            for (int i = 0; i < jsonArray.length(); i++) {
+//                                menggunakan perulangan arraylist
+                                modelku = new Model();
+                                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                                modelku.setOriginal_title(jsonObject.getString("original_title"));
+                                modelku.setRelease_date(jsonObject.getString("realease_date"));
+                                modelku.setPoster_path("https://image.tmdb.org/t/p/w500"+jsonObject.getString("poster_path"));
+                                modelku.setOverview(jsonObject.getString("overview"));
+                                modelku.setAdult(jsonObject.getBoolean("adult"));
+                                modelku.setVote_count(jsonObject.getInt("vote_count"));
+                                DataArrayList.add(modelku);
+                            }
+
+                            adapter = new DataAdapter(DataArrayList, new DataAdapter.Callback() {
+                                @Override
+                                public void onClick(int position) {
+
+                                }
+
+                                @Override
+                                public void test() {
+
+                                }
+                            });
+                            RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(ListData.this);
+                            recyclerView.setLayoutManager(layoutManager);
+                            recyclerView.setAdapter(adapter);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+
+
+                    @Override
+                    public void onError(ANError error) {
+                        // handle error
+                        Log.d("errorku", "onError errorCode : " + error.getErrorCode());
+                        Log.d("errorku", "onError errorBody : " + error.getErrorBody());
+                        Log.d("errorku", "onError errorDetail : " + error.getErrorDetail());
+                    }
+                });
     }
 }
