5a849e9a69c3778872094844bcde792ee9b81644
NadiaAqmarina <nadia.10rpl2.2019@gmail.com>
2020-09-05 21:38:24 +0700

    add json parsing

--- app/src/main/java/com/example/richa11rpl22019/Mainmenu.java
+++ app/src/main/java/com/example/richa11rpl22019/Mainmenu.java
@@ -1,14 +1,24 @@
 package com.example.richa11rpl22019;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Mainmenu extends AppCompatActivity {
-
+    CardView menu1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainmenu);
+        menu1 = (CardView)findViewById(R.id.cardku);
+        menu1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), ListData.class));
+            }
+        });
     }
 }
